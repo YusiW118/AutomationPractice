594294f193322f3621aefe43561964cf4533c8d5
YusiW118 <yuzhi.wu118@gmail.com>
2022-09-24 16:24:30 +0200

    comment

--- src/main/java/utilities/Constants.java
+++ src/main/java/utilities/Constants.java
@@ -14,5 +14,7 @@ public class Constants {
 	public static final String USERNAME="username";
 	public static final String BROWSER="browser";
 	public static final String CART_QUANTITY_TEST="2 Products";
+	/* safas */
+	/* fniengiengieni */
 
 }
